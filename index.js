e7fdde447c234b56da962713279b124b2086accb
vhosurtest <vasudeva.hosur@outlook.com>
2020-10-25 17:39:51 +0530

    Added cli-handle-error for handling errors and optinally exiting nodejs process

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 #!/usr/bin/env node
 
 const init = require('./utils/init');
+const handleError = require('cli-handle-error');
 
 (async () => {
     init();
+    const err = new Error('Something went wrong');
+    // Syntax: handleError(err_name, err_obj, show_error_obj, exit_node_process);
+    handleError('Custom Error', err, true, true);
     console.log(`Hi! I'm vhosur!`);
 })();
